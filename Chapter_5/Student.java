f8948b736401d2747ce59e819f6f8a137a610d7a
Sann-Htet <sannhtet899@gmail.com>
2025-07-28 18:50:47 +0630

    static member variables and instance variables

--- Chapter_5/Student.java
+++ Chapter_5/Student.java
@@ -4,20 +4,27 @@ public class Student {
     private String name;        // Student's name.
     public double test1, test2, test3;  // Grades on three tests.
 
+    private int ID;     // Unique ID number for this student.
+    private static int nextUniqueID = 0;    // keep track of next available unique ID number
+
     public Student(String theName) {
         // Constructor for Student objects;
         // provides a name for the Student.
-        // The name can't be null.
-        if (theName == null) {
-            throw new IllegalArgumentException("name can't be null");
-        }
+        // and assigns the student a unique ID number.
         name = theName;
+        nextUniqueID++;
+        ID = nextUniqueID;
     }
 
     public String getName() {
         // Getter method for reading the value of the private
         // instance variable, name.
         return name;
+    }
+
+    public int getID() {
+        // Getter method for reading the value of ID.
+        return ID;
     }
 
     public double getAverage() {
