46ece63f1121d5057b87d34ba14f674d9c6f65f8
Sann-Htet <sannhtet899@gmail.com>
2025-08-29 18:24:07 +0630

    default methods in interface

---
+++ Chapter_5/Readable.java
@@ -0,0 +1,16 @@
+package Chapter_5;
+
+public interface Readable {     // represents a source of input
+    
+    public char readChar();     // read the next character from the input
+
+    default public String readLine() { // read up to the next line feed
+        StringBuilder line = new StringBuilder();
+        char ch = readChar();
+        while (ch != '\n') {
+            line.append(ch);
+            ch = readChar();
+        }
+        return line.toString();
+    }
+}
---
+++ Chapter_5/Stars.java
@@ -0,0 +1,18 @@
+package Chapter_5;
+
+public class Stars implements Readable {
+
+    public char readChar() {
+        if (Math.random() > 0.02)
+            return '*';
+        else
+            return '\n';
+    }
+    public static void main(String[] args) {
+        Stars stars = new Stars();
+        for (int i = 0 ; i < 10; i++ ) {
+            String line = stars.readLine(); // Calls the default method!
+            System.out.println( line );
+        }
+    }
+}
